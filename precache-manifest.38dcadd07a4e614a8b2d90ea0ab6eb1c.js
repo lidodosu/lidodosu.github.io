6d164ab0096ffd040415064afc4943e22217d242
Your Name <you@example.com>
2019-03-16 22:00:20 +0300

    drawChart v1

---
+++ precache-manifest.38dcadd07a4e614a8b2d90ea0ab6eb1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a3d97c77a1fa636ba6b",
+    "url": "/static/css/main.acbb2df1.chunk.css"
+  },
+  {
+    "revision": "7a3d97c77a1fa636ba6b",
+    "url": "/static/js/main.2b359d3a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "28b6d9c4330ba63e9aa8",
+    "url": "/static/js/2.fc74d1ea.chunk.js"
+  },
+  {
+    "revision": "3e494d795da254900f1343df25216daa",
+    "url": "/index.html"
+  }
+];
